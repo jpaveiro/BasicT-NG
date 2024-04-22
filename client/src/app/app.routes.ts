9c7d683aaf5bdbb8f90bae83242785b9f690a2c7
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-22 20:19:28 -0300

    feat: register route & component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   {path: "", component: LoginComponent, title: "Login"},
-  {path: "home", component: HomeComponent, title: "Home"},
+  {path: "home", component: HomeComponent, title: "Inicio"},
+  {path: "register", component: RegisterComponent, title: "Registrar"},
 ];
