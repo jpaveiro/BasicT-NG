939ef08da8dbef78237ff9e8592a807ab2a428ed
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-22 19:44:13 -0300

    feat: login page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-  {path: "home", component: HomeComponent, title: "Home"}
+  {path: "", component: LoginComponent, title: "Login"},
+  {path: "home", component: HomeComponent, title: "Home"},
 ];
---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements AfterViewInit {
+  password: string = "";
+  seePassword: boolean = false;
+
+  ngAfterViewInit() {
+    this.posicionarIcone("password", "visibility_icon");
+  }
+
+  setPassword(event: any) {
+    this.password = event.target.value;
+  }
+
+  posicionarIcone(inputId: string, iconId: string) {
+    const input = document.getElementById(inputId);
+    const icon = document.getElementById(iconId);
+    
+    if (input && icon) {
+      const inputRect = input.getBoundingClientRect();
+      const inputTop = inputRect.top;
+      const inputRight = inputRect.right;
+      
+      icon.style.top = inputTop + 'px';
+      icon.style.right = (window.innerWidth - inputRight) + 'px';
+    }
+  }
+
+  toggleSeePassword() {
+    const icon = document.getElementById("visibility_icon");
+    this.seePassword = !this.seePassword;
+    if (icon) {
+      icon.textContent = this.seePassword ? "visibility" : "visibility_off";
+    }
+  }
+  
+}
