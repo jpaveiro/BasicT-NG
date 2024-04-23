034ad7e1ae363fb329ca6bef46b6aad24a9a4995
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-04-22 22:21:38 -0300

    feat: specific response for user login

---
+++ server/src/main/java/com/gjv/basicTapi/model/UserResponse.java
@@ -0,0 +1,15 @@
+package com.gjv.basicTapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private String token;
+    private String name;
+}
