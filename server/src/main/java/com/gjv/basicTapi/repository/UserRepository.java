32dbf3b0d43577f7e5f5bdbc9fd0636e0ec68c5b
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-04-22 22:21:53 -0300

    fix: getuser type

--- server/src/main/java/com/gjv/basicTapi/repository/UserRepository.java
+++ server/src/main/java/com/gjv/basicTapi/repository/UserRepository.java
@@ -6,13 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
     @Query("SELECT u FROM User u WHERE u.email = :email AND u.password = :password")
-    long getUser(
+    User getUser(
             @Param("email") String email,
             @Param("password") String password
     );
