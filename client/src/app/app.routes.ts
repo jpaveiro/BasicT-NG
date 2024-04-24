65cfcf9f73ee7a2a87962d8c35e7994c6e109090
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-23 23:29:07 -0300

    chore: AuthGuard implementation

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
   {path: "", component: LoginComponent, title: "Login"},
-  {path: "home", component: HomeComponent, title: "Inicio"},
+  {path: "home", component: HomeComponent, title: "Inicio", canActivate: [AuthGuard]},
   {path: "register", component: RegisterComponent, title: "Registrar"},
 ];
