55ffed7194edba7d75998a3e1595aa8d8a7671e5
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-23 23:10:29 -0300

    chore: CORS usage

---
+++ server/src/main/java/com/gjv/basicTapi/config/ApiConfig.java
@@ -0,0 +1,16 @@
+package com.gjv.basicTapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiConfig implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry corsRegistry) {
+    corsRegistry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedHeaders("*");
+  }
+}
