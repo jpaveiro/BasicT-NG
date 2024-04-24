fa0f851407ff1084d50edd5ae74b24ee42b3ed01
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-23 23:28:35 -0300

    feat: guard context

---
+++ client/src/app/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { CookieService } from "ngx-cookie-service";
+
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthGuard implements CanActivate {
+  constructor(private cookieService: CookieService, private router: Router) {}
+
+  canActivate(): boolean {
+    const token = this.cookieService.get("basict:user-token");
+    const name = localStorage.getItem("name");
+    if (token || name) {
+      return true;
+    } else {
+      this.router.navigate(["/login"]);
+      return false;
+    }
+  }
+}
