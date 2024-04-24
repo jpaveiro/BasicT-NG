652ece679bac74bd256bd2c92b8622346256aae5
viniciusdevjesus <viniciusjesusfilho07@gmail.com>
2024-04-23 21:26:37 -0300

    feat: Put route

--- server/src/main/java/com/gjv/basicTapi/controller/UserController.java
+++ server/src/main/java/com/gjv/basicTapi/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.gjv.basicTapi.controller;
 
+import com.gjv.basicTapi.dto.EditRequestDto;
 import com.gjv.basicTapi.dto.LoginRequestDto;
 import com.gjv.basicTapi.dto.UserRequestDto;
 import com.gjv.basicTapi.model.User;
@@ -9,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +40,16 @@ public class UserController {
     {
         long startTime = System.currentTimeMillis();
         ResponseEntity<?> response = userService.login(request);
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+        LOGGER.info("Elapsed time: " + elapsedTime + " milisseconds.");
+        return response;
+    }
+    @PutMapping("/v1/edit")
+    public ResponseEntity<?> editUser(@RequestBody EditRequestDto request)
+    {
+        long startTime = System.currentTimeMillis();
+        ResponseEntity<?> response = userService.editUser(request);
         long endTime = System.currentTimeMillis();
         long elapsedTime = endTime - startTime;
         LOGGER.info("Elapsed time: " + elapsedTime + " milisseconds.");
