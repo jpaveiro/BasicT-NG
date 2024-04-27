3be5b93fbbbc497b20160b411d548c60057d7b10
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-27 17:44:38 -0300

    fix: not nullable data

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -3,33 +3,33 @@ CREATE DATABASE BasicT_database;
 USE BasicT_database;
 
 CREATE TABLE Users(
-	id VARCHAR(255),
-	name CHAR(255),
-	cellphone VARCHAR(20) UNIQUE,
-	email	VARCHAR(255) UNIQUE,
-	cpf VARCHAR(11) UNIQUE,
-	rg VARCHAR(20),
-	state_rg VARCHAR(6),
-	birth_date DATE,
-	password VARCHAR(255),
+	id VARCHAR(255) NOT NULL,
+	name CHAR(255) NOT NULL,
+	cellphone VARCHAR(20) UNIQUE NOT NULL,
+	email	VARCHAR(255) UNIQUE NOT NULL,
+	cpf VARCHAR(11) UNIQUE NOT NULL,
+	rg VARCHAR(20) NOT NULL,
+	state_rg VARCHAR(6) NOT NULL,
+	birth_date DATE NOT NULL,
+	password VARCHAR(255) NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE Products(
-	id_product VARCHAR(255),
-	name VARCHAR(255),
-	prod_quantity	INTEGER,
-	price	NUMERIC(8, 2),
+	id_product VARCHAR(255) NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	prod_quantity	INTEGER NOT NULL,
+	price	NUMERIC(8, 2) NOT NULL,
 	PRIMARY KEY (id_product)
 );
 
 CREATE TABLE Purchases(
-	id_purchase	VARCHAR(255),
-	id_user VARCHAR(255),
-	id_prod VARCHAR(255),
-  prod_quantity	INTEGER,
-	purchase_date	DATETIME,
-	total_amount NUMERIC(8,2),
+	id_purchase	VARCHAR(255) NOT NULL,
+	id_user VARCHAR(255) NOT NULL,
+	id_prod VARCHAR(255) NOT NULL,
+  prod_quantity	INTEGER NOT NULL,
+	purchase_date	DATETIME NOT NULL,
+	total_amount NUMERIC(8,2) NOT NULL,
 	PRIMARY KEY(id_purchase),
   FOREIGN KEY (id_user) REFERENCES Users(id),
   FOREIGN KEY (id_prod) REFERENCES Products(id_product)
