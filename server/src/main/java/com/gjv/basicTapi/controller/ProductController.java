02f51e75d24d28cfa586c30d56bbb49d029c6697
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-04-27 02:10:02 -0300

    feat: getProduct route

--- server/src/main/java/com/gjv/basicTapi/controller/ProductController.java
+++ server/src/main/java/com/gjv/basicTapi/controller/ProductController.java
@@ -1,18 +1,13 @@
 package com.gjv.basicTapi.controller;
 
-import com.gjv.basicTapi.dto.AddProductRequestDto;
-import com.gjv.basicTapi.model.Product;
-import com.gjv.basicTapi.model.User;
+import com.gjv.basicTapi.dto.GetProductRequestDto;
+import com.gjv.basicTapi.dto.SetProductRequestDto;
 import com.gjv.basicTapi.usecase.ProductService;
-import com.gjv.basicTapi.usecase.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
@@ -24,10 +19,21 @@ public class ProductController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
 
     @PostMapping("/v1/set")
-    public ResponseEntity<?> setProduct(@RequestBody AddProductRequestDto request)
+    public ResponseEntity<?> setProduct(@RequestBody SetProductRequestDto request)
     {
         long startTime = System.currentTimeMillis();
         ResponseEntity<?> response = productService.setProduct(request);
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+        LOGGER.info("Elapsed time: " + elapsedTime + " milisseconds.");
+        return response;
+    }
+
+    @GetMapping("/v1/get")
+    public ResponseEntity<?> getProduct(@RequestBody GetProductRequestDto request)
+    {
+        long startTime = System.currentTimeMillis();
+        ResponseEntity<?> response = productService.getProduct(request);
         long endTime = System.currentTimeMillis();
         long elapsedTime = endTime - startTime;
         LOGGER.info("Elapsed time: " + elapsedTime + " milisseconds.");
