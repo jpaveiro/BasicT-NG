b1e986a422a5923f38a6879c50a87fc84dad5ca9
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-27 18:57:25 -0300

    fix: @Service annotation on top of PurchaseService entity

--- server/src/main/java/com/gjv/basicTapi/usecase/PurchaseService.java
+++ server/src/main/java/com/gjv/basicTapi/usecase/PurchaseService.java
@@ -8,7 +8,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PurchaseService {
   @Autowired
   private PurchaseRepository purchaseRepository;
