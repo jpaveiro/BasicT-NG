cd2be75cce8f4761ceeccfe3588f8d5fba4a5eb9
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-04-27 01:43:06 -0300

    feat: repository for product

---
+++ server/src/main/java/com/gjv/basicTapi/repository/ProductRepository.java
@@ -0,0 +1,29 @@
+package com.gjv.basicTapi.repository;
+
+import com.gjv.basicTapi.model.Product;
+import com.gjv.basicTapi.model.User;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> {
+    @Query("SELECT u FROM Product u WHERE u.idProduct = :idProduct")
+    User getProduct(
+            @Param("idProduct") String idProduct
+    );
+
+    @Modifying
+    @Transactional
+    @Query("INSERT INTO Product(idProduct, name, prodQuantity, price) " +
+            "VALUES (:idProduct, :name, :prodQuantity, :price)")
+    void setProduct(
+            @Param("idProduct") String idProduct,
+            @Param("name") String name,
+            @Param("prodQuantity") int productQuantity,
+            @Param("price") double price
+    );
+}
