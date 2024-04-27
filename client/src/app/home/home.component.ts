f7b39d933fa66940fe094055dd6369c45227b173
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-26 22:07:24 -0300

    feat: home screen initialization

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  userName: string = "";
 
+  constructor(private cookieService: CookieService) {
+    this.userName = this.capitalizeName(localStorage.getItem("user-name") ?? "");
+  }
+
+  capitalizeName(fullName: string) {
+    const splitedName = fullName.toLowerCase().split(" ");
+    const capitalizedName = splitedName.map((name) => {
+      return name.charAt(0).toUpperCase() + name.slice(1);
+    });
+    return capitalizedName.join(" ");
+  }
+
+  logout() {
+    localStorage.removeItem("user-name");
+    this.cookieService.deleteAll();
+    location.href = "/";
+  }
 }
