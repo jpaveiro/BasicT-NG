a57a46bd95f6e4fd4493aacf7fd11298e2318c4d
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-04-27 01:39:00 -0300

    feat: request model for add product

---
+++ server/src/main/java/com/gjv/basicTapi/dto/AddProductRequestDto.java
@@ -0,0 +1,17 @@
+package com.gjv.basicTapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddProductRequestDto {
+    private String idProduct;
+    private String name;
+    private Integer prodQuantity;
+    private Double price;
+}
