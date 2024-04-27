e268d6ac12c9b43e384697942b151de508a03f1b
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-04-26 22:30:19 -0300

    feat: Product class

---
+++ server/src/main/java/com/gjv/basicTapi/model/Product.java
@@ -0,0 +1,34 @@
+package com.gjv.basicTapi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Products")
+public class Product {
+    @Id
+    @Column(name = "id_product")
+    private String idProduct;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "prod_quantity")
+    private int prodQuantity;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "status")
+    private String status;
+}
