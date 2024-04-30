808ae8fc6ddf0438f0f85c96c31f6b0a54292060
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-29 22:09:18 -0300

    feat: new-product route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,9 +4,14 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AuthGuard } from './auth.guard';
 import { NewSaleComponent } from './components/new-sale/new-sale.component';
+import { NewProductComponent } from './components/new-product/new-product.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent, title: 'Login' },
+  { 
+    path: '', 
+    component: LoginComponent, 
+    title: 'Login' 
+  },
   {
     path: 'home',
     component: HomeComponent,
@@ -19,5 +24,14 @@ export const routes: Routes = [
     title: 'Nova Venda',
     canActivate: [AuthGuard],
   },
-  { path: 'register', component: RegisterComponent, title: 'Registrar' },
+  { 
+    path: 'register', 
+    component: RegisterComponent,
+    title: 'Registrar'
+  },
+  {
+    path: 'product/new',
+    component: NewProductComponent,
+    title: 'Novo produto',
+  },
 ];
