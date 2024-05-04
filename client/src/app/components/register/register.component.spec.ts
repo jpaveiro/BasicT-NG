6fff6556734bc5de3cf63317d3c5791cc3f67b7f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-04 17:37:20 -0300

    test: register component jest

--- client/src/app/components/register/register.component.spec.ts
+++ client/src/app/components/register/register.component.spec.ts
@@ -1,64 +1,49 @@
-import { RegisterComponent } from './register.component';
+import { TestBed } from "@angular/core/testing";
+import { RegisterComponent } from "./register.component"
+import { CookieService } from "ngx-cookie-service";
+
 
 describe('RegisterComponent', () => {
-  let component: RegisterComponent;
+  let registerComponent: RegisterComponent;
+  let cookieService: CookieService;
 
   beforeEach(() => {
-    component = new RegisterComponent();
+    TestBed.configureTestingModule({
+      providers: [RegisterComponent, CookieService]
+    });
+
+    registerComponent = TestBed.inject(RegisterComponent);
+    cookieService = TestBed.inject(CookieService);
+
+    cookieService.deleteAll();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be created', () => {
+    expect(registerComponent).toBeTruthy();
   });
 
-  it('should initialize variables properly', () => {
-    expect(component.password).toEqual('');
-    expect(component.secondPassword).toEqual('');
-    expect(component.seePassword).toBe(false);
-    expect(component.overlay).toBe(false);
-    expect(component.adminEmail).toEqual('');
-    expect(component.adminPassword).toEqual('');
+  it('should block access', () => {
+    expect(registerComponent.isLogged()).toBe(false);
   });
 
-  it('should set email in lowercase on setAdminEmail', () => {
-    component.setAdminEmail({ target: { value: 'Test@Example.com' } });
-    expect(component.adminEmail).toEqual('test@example.com');
-  });
+  it('should allow access', () => {
+    cookieService.set('basict:user-token', '12345678');
 
-  it('should convert stateRg to uppercase on setStateRg', () => {
-    component.setStateRg({ target: { value: 'sp' } });
-    expect(component.stateRg).toEqual('SP');
-  });
+    expect(registerComponent.isLogged()).toBe(true);
+  })
 
-  it('should set second password on setSecondPassword', () => {
-    component.setSecondPassword({ target: { value: 'password' } });
-    expect(component.secondPassword).toEqual('password');
-  });
+  it('should convert email and stateRg case', () => {
+    const email: string = 'ADMIN@ADMIN.org';
+    const stateRg: string = 'sSp/Sp';
 
-  it('should set name on setName', () => {
-    component.setName({ target: { value: 'John Doe' } });
-    expect(component.name).toEqual('John Doe');
-  });
+    registerComponent.handleEmail({target: {value: email}})
+    registerComponent.setStateRg({target: {value: stateRg}})
 
-  it('should set overlay to true on checkInformations if all fields are filled', () => {
-    component.password = 'password';
-    component.secondPassword = 'password';
-    component.cpf = '123.456.789-01';
-    component.cellphone = '(12) 34567-8901';
-    component.email = 'test@example.com';
-    component.bday = '1990-01-01';
-    component.checkInformations();
-    expect(component.overlay).toBe(true);
-  });
+    expect(registerComponent.email).toBe('admin@admin.org');
+    expect(registerComponent.stateRg).toBe('SSP/SP')
+  })
 
-  it('should not set overlay to true on checkInformations if any field is missing', () => {
-    component.password = 'password';
-    component.secondPassword = 'password';
-    component.cpf = '123.456.789-01';
-    component.cellphone = '(12) 34567-8901';
-    component.email = 'test@example.com';
-    // Missing birthday
-    component.checkInformations();
-    expect(component.overlay).toBe(false);
+  it('should return error from empty fields or diferent passwords', () => {
+    expect(registerComponent.checkInformations()).toBe(false);
   });
-});
+})
