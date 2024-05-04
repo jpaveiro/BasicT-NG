adec2ba025a06ab15d5553a1b483babf197e6e63
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-04 17:14:41 -0300

    feat: admin guard

--- client/src/app/guards/admin/admin.guard.ts
+++ client/src/app/guards/admin/admin.guard.ts
@@ -1,5 +1,21 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { CookieService } from "ngx-cookie-service";
 
-export const adminGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: "root"
+})
+export class AdminGuard implements CanActivate {
+  constructor(private cookieService: CookieService, private router: Router) { }
+
+  canActivate(): boolean{
+    const superToken = this.cookieService.get("basict:super-user-token");
+      
+    if (!superToken) {
+      this.router.navigate(["/home"]);
+      return false;
+    }
+
+    return true;
+  }
+}
