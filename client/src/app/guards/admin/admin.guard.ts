3ccb665d526ca8b3d0c47f6cb1b5566b002c9395
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-04 17:56:08 -0300

    refactor: inver code logic

--- client/src/app/guards/admin/admin.guard.ts
+++ client/src/app/guards/admin/admin.guard.ts
@@ -1,21 +1,22 @@
-import { Injectable } from "@angular/core";
-import { CanActivate, Router } from "@angular/router";
-import { CookieService } from "ngx-cookie-service";
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class AdminGuard implements CanActivate {
-  constructor(private cookieService: CookieService, private router: Router) { }
+  constructor(private cookieService: CookieService, private router: Router) {}
 
-  canActivate(): boolean{
-    const superToken = this.cookieService.get("basict:super-user-token");
-      
-    if (!superToken) {
-      this.router.navigate(["/home"]);
-      return false;
+  canActivate(): boolean {
+    const superToken = this.cookieService.get('basict:super-user-token');
+
+    if (superToken) {
+      return true;
     }
 
-    return true;
+    alert('Você não tem permissão para acessar essa página');
+    this.router.navigate(['/home']);
+    return false;
   }
 }
