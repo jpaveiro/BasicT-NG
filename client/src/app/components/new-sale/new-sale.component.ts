04b8c7a44d228168562701f3b2ce14f208498b5b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-05 15:13:42 -0300

    feat: payment method on new sale

--- client/src/app/components/new-sale/new-sale.component.ts
+++ client/src/app/components/new-sale/new-sale.component.ts
@@ -6,11 +6,12 @@ import { env } from '../../../../config/enviroments';
 import { CookieService } from 'ngx-cookie-service';
 import { ProductsToSellTableRequest } from '../../interfaces/productsToSellTableRequest.interface';
 import { capitalize } from '../../util/capitalize.util';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-sale',
   standalone: true,
-  imports: [CommonModule, LoaderComponent],
+  imports: [CommonModule, LoaderComponent, FormsModule],
   templateUrl: './new-sale.component.html',
   styleUrl: './new-sale.component.scss'
 })
@@ -23,12 +24,17 @@ export class NewSaleComponent {
   purchaseCode: string = "";
   userId: string = "";
   productIds: string[] = [];
+  selectedPaymentMethod: string = "dinheiro";
 
   allData: ProductsToSellTableRequest[] = [];
 
   constructor(private cookieService: CookieService) {
     this.purchaseCode = this.generatePurchaseCode();
     this.userId = this.cookieService.get("basict:user-id");
+  }
+
+  setSelectedPaymentMethod(event: any) {
+    console.log(this.selectedPaymentMethod);
   }
 
   generatePurchaseCode(): string {
@@ -50,7 +56,8 @@ export class NewSaleComponent {
           userId: this.userId,
           quantity: product.quantity,
           productId: productId,
-          totalAmount: product.price
+          totalAmount: product.price,
+          paymentMethod: this.selectedPaymentMethod.toUpperCase(),
         };
         await axios.post(env.apiUrl + "/purchase/v1/sell", params);
         alert("Você vendeu!");
