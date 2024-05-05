0030285dce09080885c6fc5476a75daec2c235da
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-05 15:26:34 -0300

    fix: verifyng the 10 tokens

--- client/src/app/guards/auth/auth.guard.ts
+++ client/src/app/guards/auth/auth.guard.ts
@@ -1,23 +1,38 @@
-import { Injectable } from "@angular/core";
-import { CanActivate, Router } from "@angular/router";
-import { CookieService } from "ngx-cookie-service";
-
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
   constructor(private cookieService: CookieService, private router: Router) {}
 
   canActivate(): boolean {
-    const token = this.cookieService.get("basict:user-token");
-    if (token) {
-      return true;
-    } else {
-      
-      alert("Você não tem permissão para acessar essa página");
-      this.router.navigate(["/"]);
+    let tokens = [];
+
+    for (let i = 0; i <= 9; i++) {
+      const token = this.cookieService.get(`basict:user-token${i}`);
+      if (token) {
+        tokens.push(true);
+      }
+    }
+
+    if (!tokens[0]) {
+      alert('Você não tem permissão para acessar essa página');
+      this.router.navigate(['/']);
       return false;
     }
+
+    for (let i = 0; i <= tokens.length; i++) {
+      if (tokens[i] == false) {
+        alert('Você não tem permissão para acessar essa página');
+        this.router.navigate(['/']);
+
+        return false;
+      }
+    }
+
+    return true;
   }
 }
