fe9ccf2c0b1c2114aa3f04ba62ea6f016264b42f
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-05-22 23:02:25 -0300

    feat: daily overview ts initialization

---
+++ client/src/app/components/daily-overview/daily-overview.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { capitalize } from '../../util/capitalize.util';
+import { LoaderComponent } from '../loader/loader.component';
+import { CommonModule } from '@angular/common';
+import { setTimeout } from 'timers/promises';
+
+@Component({
+  selector: 'app-daily-overview',
+  standalone: true,
+  imports: [LoaderComponent, CommonModule],
+  templateUrl: './daily-overview.component.html',
+  styleUrl: './daily-overview.component.scss',
+})
+export class DailyOverviewComponent {
+  userName: string = '';
+  loader: boolean = false;
+
+  constructor(private cookieService: CookieService) {
+    this.userName = capitalize(localStorage.getItem('user-name') ?? '');
+    this.request();
+  }
+
+  isAdminLogged(): boolean {
+    const superToken = this.cookieService.get('basict:super-user-token');
+
+    if (superToken) {
+      return true;
+    }
+
+    return false;
+  }
+
+  goBack()
+  {
+    window.history.back();
+  }
+
+  async request()
+  {
+    this.loader = true;
+    this.loader = false;
+  }
+}
