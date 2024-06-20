15ba5b9688fc69b8b6449ab3a1adc4635223b234
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-06-20 15:52:04 -0300

    refactor: Unified Product DTOs in a single one Product Request Dto.

---
+++ server/src/main/java/com/gjv/basicTapi/dto/ProductRequestDto.java
@@ -0,0 +1,16 @@
+package com.gjv.basicTapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductRequestDto {
+    private String idProduct;
+    private String name;
+    private Double price;
+}
