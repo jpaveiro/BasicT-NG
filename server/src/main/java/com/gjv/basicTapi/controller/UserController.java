f3e694a0bd6bd2ce744631acfda5c448d4f249d5
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-06-20 15:27:12 -0300

    chore: unnusual imports removed.

--- server/src/main/java/com/gjv/basicTapi/controller/UserController.java
+++ server/src/main/java/com/gjv/basicTapi/controller/UserController.java
@@ -8,7 +8,6 @@ import com.gjv.basicTapi.usecase.UserService;
 import com.gjv.basicTapi.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
