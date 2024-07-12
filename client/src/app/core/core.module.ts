ee5aa3efc084c855d772b0e69b12989065c4c070
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-12 20:09:45 -0300

    feat: custom button at header component

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -4,10 +4,16 @@ import { HeaderComponent } from './components/header/header.component';
 import { LucideAngularModule, Menu } from 'lucide-angular';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '../app-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, LucideAngularModule.pick({ Menu }), AppRoutingModule],
+  imports: [
+    CommonModule,
+    LucideAngularModule.pick({ Menu }),
+    AppRoutingModule,
+    SharedModule,
+  ],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
