641d75dff416754afc4bc55f626b8c2882dd179f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-12 23:27:36 -0300

    feat: auth guard

---
+++ client/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) {}
+
+  canActivate(): boolean {
+    const id = this.authService.id;
+    const email = this.authService.name;
+
+    if (!id || !email) {
+      this.router.navigate(['/login']);
+      return false;
+    }
+
+    return true;
+  }
+}
