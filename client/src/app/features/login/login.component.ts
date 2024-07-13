d2c44b88600f7f05fe3bf8164102ab73a166d5b4
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-12 21:09:38 -0300

    feat: login page

---
+++ client/src/app/features/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  userForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+
+  constructor(private readonly toastr: ToastrService) {}
+
+  onSubmit(): void {
+    if (this.userForm.invalid) {
+      if (this.userForm.get('email')?.hasError('email')) {
+        this.toastr.error('Insira um email válido.', 'Erro!');
+        return;
+      }
+
+      if (this.userForm.get('password')?.hasError('minlength')) {
+        this.toastr.error(
+          'A senha precisa ter no minimo 8 caractéres.',
+          'Erro!'
+        );
+        return;
+      }
+
+      console.log(this.userForm.get('password'));
+
+      this.toastr.error('Todos os dados são obrigatórios.', 'Atenção');
+      return;
+    }
+  }
+}
--- client/src/app/features/login/login.module.ts
+++ client/src/app/features/login/login.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { LoginComponent } from './login.component';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
+import { CoreModule } from '../../core/core.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        title: 'basicT | Login',
+        component: LoginComponent,
+      },
+    ]),
+    SharedModule,
+    CoreModule,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}
