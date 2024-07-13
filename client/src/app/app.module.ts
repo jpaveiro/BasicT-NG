287b939df35c1322040408ce27383c377a156c3a
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-12 23:12:46 -0300

    chore: providing httpClient

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { SharedModule } from './shared/shared.module';
 import { FeaturesModule } from './features/features.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -26,7 +27,7 @@ import { ToastrModule } from 'ngx-toastr';
     SharedModule,
     FeaturesModule,
   ],
-  providers: [provideClientHydration()],
+  providers: [provideClientHydration(), provideHttpClient()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
