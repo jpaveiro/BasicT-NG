e980b2202c8b1e863dc29f43fb5f8846bec4456e
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-12 23:13:48 -0300

    refactor: remove unused import

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { LucideAngularModule, Menu } from 'lucide-angular';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '../app-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
