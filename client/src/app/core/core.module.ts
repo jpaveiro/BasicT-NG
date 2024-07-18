d0785cdf3094480f44c09e4c2a83870c1d8552b0
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-17 22:32:01 -0300

    chore: implementation of loader component

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { LucideAngularModule, Menu } from 'lucide-angular';
+import { Loader, LucideAngularModule, Menu } from 'lucide-angular';
 import { AppRoutingModule } from '../app-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, LoaderComponent],
   imports: [
     CommonModule,
-    LucideAngularModule.pick({ Menu }),
+    LucideAngularModule.pick({ Menu, Loader }),
     AppRoutingModule,
     SharedModule,
   ],
-  exports: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent, LoaderComponent],
 })
 export class CoreModule {}
